12f1bd1faa7a7b8eeb657dbc23b0c9be5394970c
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-12-10 01:15:20 +0500

    MoveLeftRightBack

---
+++ MoveBackLeftRight.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 45f;
+
+
+    void Update()
+    {
+        float horizontalInput = 0f;
+        float verticalInput = 0f;
+
+        if (Input.GetKey("w"))
+        {
+            verticalInput = 1f;
+        }
+        else if (Input.GetKey("s"))
+        {
+            verticalInput = -1f;
+        }
+
+        Vector3 movement = new Vector3(0, 0, verticalInput);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+        {
+            horizontalInput = -1f;
+        }
+        else if (Input.GetKey("d"))
+        {
+            horizontalInput = 1f;
+        }
+
+        Vector3 rotation = new Vector3(0, horizontalInput, 0);
+        transform.Rotate(rotation * rotationSpeed * Time.deltaTime);
+    }
+
+}
